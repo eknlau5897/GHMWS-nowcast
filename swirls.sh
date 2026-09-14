20ab1e2ac193d7fb0b745a29c5fab47d432d993f
eknlau5897 <eknlau@gmail.com>
2026-09-14 19:45:34 +0800

    update

--- swirls.sh
+++ swirls.sh
@@ -23,7 +23,7 @@ echo "--- 任務開始: $(date) ---"
 # ==============================================================================
 # 1. PYTHON HKO RADAR ANALYSIS MATRIX
 # ==============================================================================
-opt/anaconda3/bin/python << EOF_PYTHON
+opt/anaconda/bin/python3.11 << EOF_PYTHON
 import os
 import pandas as pd 
 import xarray as xr
